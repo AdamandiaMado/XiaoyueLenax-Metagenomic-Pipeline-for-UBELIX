7448f342648e5e4be623345e669ab0fc5f0cb66a
XiaoyueLenax <corufang51@gmail.com>
2024-02-06 01:10:19 +0100

    ac

--- breakdown/qc.sh
+++ breakdown/qc.sh
@@ -10,12 +10,13 @@
 #SBATCH --partition=epyc2
 #SBATCH --array=0-6
 
+
 # ===========================================================================
 
 #           Module setup - Loeading all required packages
 
 #============================================================================
-    #module load Anaconda3
+
     
     #   Load all vital-IT packages preinstalled
         # metawrap is the placeholder name for your metawrap envirnment! 
@@ -29,19 +30,19 @@
 #                       User Input: Sets working directory
 
 # ====================================================================================
-WORKDIR=""   # Master directory where you want everything to be in there, placeholder
-RAW_DATA_DIR=""   # Raw data directory where the raw fastq files are , placeholder
-        #WARNING: make sure your data is in _R1.fastq format!
+    WORKDIR=""   # Master directory where you want everything to be in there, placeholder
+    RAW_DATA_DIR=""   # Raw data directory where the raw fastq files are , placeholder
+            #WARNING: make sure your data is in _1.fastq format!
 
-# Replace these with your sample names so it can be analysed automatically in a loop.        
-sample_names=("Human1" "Human2" "Human3") # Recommend to manually type in all your sample names to loop through later..
-REF_GENOME="/storage/scratch/users/xd22m086/04_metawrap_testground/DATABASE/UHGG_reps.fasta" 
+    # Replace these with your sample names so it can be analysed automatically in a loop.        
+    sample_names=("Human1" "Human2" "Human3") # Recommend to manually type in all your sample names to loop through later.. placeholder
+    REF_GENOME="/storage/scratch/users/xd22m086/04_metawrap_testground/DATABASE/UHGG_reps.fasta"  # change it with your reference, placeholder
 
 
 
 # Step 1 - FastQC--------------------------------------------------------------------------------------------------------------
 
-#       The raw reads must be named with suffix *_R_1.fastq and *_R_2.fastq !!
+#       The raw reads must be named with suffix *_1.fastq and *_2.fastq !!
 for sample_id in "${sample_names[@]}"; do
     metawrap read_qc -1 $RAW_DATA_DIR/${sample_id}_R_1.fastq -2 -1 $RAW_DATA_DIR/${sample_id}_R_2.fastq -t 24 -o ${OUT_DIR}
     #Options:
